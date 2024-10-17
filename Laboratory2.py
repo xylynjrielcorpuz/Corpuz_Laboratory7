5f36f22419bdf05b9ae19d3ca354000d58e557e7
Xylyn Jriel A. Corpuz <laurenceherrera90@gmail.com>
2024-10-17 09:47:43 +0800

    Laboratory 7

---
+++ Laboratory2.py
@@ -0,0 +1,15 @@
+fname = "Xylyn Jriel"
+lname = "Corpuz"
+age= 18
+fullname = fname + " " + lname
+
+print("Hello, my name is" + " " + fullname + " " + "I am" + " " + str(age) + " " + "years old")
+
+        
+if (fullname.isuppper == True):
+    print(fullname)
+    
+else:
+    new_full = fullname.upper   
+    print(new_full)
+
---
+++ roundoff.py
@@ -0,0 +1,3 @@
+hotdog= 2.7777666
+hotdog_rounded= round(hotdog, 2)
+print(hotdog_rounded)
